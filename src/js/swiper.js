9c67385defee36aab8e861653f2a7209a19198b4
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-01-22 13:53:50 +0000

    q

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,28 +1,26 @@
 // Initialize Swiper
 
-
+ 
     var swiper = new Swiper(".mySwiperGallery", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     });
+ 
 
-
-
+  
     var swiper = new Swiper(".mySwiperRewiews", {
-        pagination: {
-            el: ".swiper-pagination",
-        },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
     });
-
+  
